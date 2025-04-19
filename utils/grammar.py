89ff16a295e4a21874719d4d2c4149f009ec2010
Botasky36 <piyapanthinpawat@gmail.com>
2025-04-19 15:32:00 +0700

    Update grammar.py

--- utils/grammar.py
+++ utils/grammar.py
@@ -1,6 +1,6 @@
 import language_tool_python
 
 def check_grammar(text):
-    tool = language_tool_python.LanguageToolPublicAPI('th-TH')
+    tool = language_tool_python.LanguageToolPublicAPI('th')
     matches = tool.check(text)
     return f"พบข้อผิดพลาด {len(matches)} จุด", matches
