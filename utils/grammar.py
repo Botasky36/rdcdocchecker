c4fbd370a194de91ef54baf66daf9b1972f9ed6d
Botasky36 <piyapanthinpawat@gmail.com>
2025-04-19 15:33:42 +0700

    Update grammar.py

--- utils/grammar.py
+++ utils/grammar.py
@@ -1,6 +1,6 @@
 import language_tool_python
 
 def check_grammar(text):
-    tool = language_tool_python.LanguageToolPublicAPI('th')
+    tool = language_tool_python.LanguageToolPublicAPI('en-US')
     matches = tool.check(text)
     return f"พบข้อผิดพลาด {len(matches)} จุด", matches
