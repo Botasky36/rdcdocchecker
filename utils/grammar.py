a6d1b4015dd80cbd40d08868293c625040e146fa
Botasky36 <piyapanthinpawat@gmail.com>
2025-04-19 15:30:07 +0700

    Update grammar.py

--- utils/grammar.py
+++ utils/grammar.py
@@ -1,6 +1,6 @@
 import language_tool_python
 
 def check_grammar(text):
-    tool = language_tool_python.LanguageTool('en-US')  # เปลี่ยนเป็น 'th' หากใช้ภาษาไทย
+    tool = language_tool_python.LanguageToolPublicAPI('th-TH')
     matches = tool.check(text)
     return f"พบข้อผิดพลาด {len(matches)} จุด", matches
